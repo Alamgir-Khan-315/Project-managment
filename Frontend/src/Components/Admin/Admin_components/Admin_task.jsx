70feb45960baaf8b6563b236d89e9a41a42c0648
Alamgir Khan <alamgirkhqwer@gmail.com>
2024-09-02 10:12:40 +0500

    Update Admin_task.jsx

--- Frontend/src/Components/Admin/Admin_components/Admin_task.jsx
+++ Frontend/src/Components/Admin/Admin_components/Admin_task.jsx
@@ -6,6 +6,7 @@ const Admin_Project = () => {
 
   const ProjectStatus = "Pending";
   const [Project, setProject] = useState([])
+  const [DetailDev, setDetailDev] = useState(false)
 
   const FetchProject = () => {
     axios.get('http://localhost:3001/GetProject')
@@ -19,6 +20,11 @@ const Admin_Project = () => {
   useEffect(() => {
     FetchProject()
   }, [])
+
+  const Detail = (id) => {
+    setDetailDev(!DetailDev)
+
+  }
 
 
   return (
@@ -59,12 +65,13 @@ const Admin_Project = () => {
               <td className='items-center text-md font-bold p-2 w-[140px]'>Assign by</td>
               <td className='items-center text-md font-bold p-2 w-[140px]'>Woring by</td>
               <td className='items-center text-md font-bold p-2 w-[100px] text-center'>Status</td>
+              <td className='items-center text-md font-bold p-2 w-[100px] text-center'>Details</td>
             </tr>
           </thead>
 
           <tbody>
             {Project.map((t, i) => {
-              return <tr className='my-2 border-blue-400 h-[50px] border-b-2'>
+              return <tr id={t._id} className='relative hover:bg-gray-900 my-2 border-blue-400 h-[50px] border-b-2'>
                 <td className=' text-center p-1'>{i + 1}</td>
                 <td className=' p-1'>{t.Title}</td>
                 <td className=' p-1'>{t.Detail}</td>
@@ -76,10 +83,19 @@ const Admin_Project = () => {
                   <div className="circle h-[10px] w-[10px] bg-blue-500 rounded-[50px]"></div>{t.Status}</td>}
                 {t.Status === "Done" && <td className='text-center rounded-lg text-green-500 flex items-center gap-2 justify-center mt-3'>
                   <div className="circle h-[10px] w-[10px] bg-green-500 rounded-[50px]"></div>{t.Status}</td>}
-
+                <td onClick={() => { Detail(id) }} className='p-1 cursor-pointer text-center'>Arrow</td>
+                {
+                  DetailDev === true &&
+                  <div className='h-[100px]'>1
+                    {t.Status === "Pending" && <h1>Project is on pending</h1>}
+                    {t.Status === "Working" && <h1>Project is in working</h1>}
+                    {t.Status === "Done" && <h1>Project has been done</h1>}
+                  </div>
+                }
               </tr>
             })
             }
+
           </tbody>
 
         </table>
